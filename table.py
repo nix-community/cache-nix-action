5234df2714c1e4290b1dfeeedb12daa19524c3ed
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-12-06 21:02:58 +0300

    fix: add cli, generate an html table

--- table.py
+++ table.py
@@ -1,36 +1,95 @@
+import sys
 import yaml
-import re
 
-action_yaml = {}
+from lxml.etree import tostring, fromstring
+from lxml.builder import E
+import markdown
+import argparse
+import textwrap
 
-with open("action.yml", "r") as action:
-    action_yaml = yaml.safe_load(action.read())
 
-header = f"| `name` | `description` | `required` | `default` | `requires` |"
-print(header)
-print("".join(map(lambda x: "|" if x == "|" else "-", list(header))))
+def convert_to_table(attrs, is_inputs=True):
+    columns = ["name", "description"]
 
-for key, value in action_yaml["inputs"].items():
-    description = value.get("description")
-    requires = (
-        match.group(1) if (match := re.search("Requires (.*).", description)) else ""
-    )
-    description_no_requires = (
-        match.group(1)
-        if (match := re.search("(.*) Requires", description))
-        else description
-    )
-    required = (
-        "`true`" if (required := value.get("required", "`false`")) == True else required
-    )
-    default = (
-        f"`{default}`"
-        if (default := value.get("default"))
-        else "`''`"
-        if default == ""
-        else ""
+    if is_inputs:
+        columns += ["default", "required"]
+
+    root = E.table(
+        E.tr(*[E.th(c) for c in columns]),
     )
 
-    print(
-        f"| `{key}` | {description_no_requires} | {required} | {default} |  {requires}"
+    for key, value in attrs.items():
+        key_pretty = f"`{key}`"
+        description = value.get("description", "")
+        columns = [key_pretty, description]
+
+        if is_inputs:
+            required = (
+                "`true`"
+                if (required := value.get("required", "`false`")) == True
+                else required
+            )
+
+            default = (
+                f"`{default}`"
+                if (default := value.get("default"))
+                else "`''`"
+                if default == ""
+                else ""
+            )
+
+            columns += [
+                default,
+                required,
+            ]
+
+        root.append(
+            E.tr(
+                *[
+                    E.td(fromstring(f"<div>{markdown.markdown(x)}</div>"))
+                    for x in columns
+                ]
+            )
+        )
+
+    return (
+        tostring(
+            root,
+            encoding="UTF-16",
+            xml_declaration=False,
+        )
+        .decode("UTF-16")
+        .replace("\n", "")
     )
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Convert `action.yaml` to HTML tables."
+    )
+
+    parser.add_argument("path", help="path to `action.yaml`")
+
+    args = parser.parse_args()
+
+    result = None
+    with open(args.path, "r") as action:
+        action_yaml = yaml.safe_load(action.read())
+
+        result = textwrap.dedent(
+            f"""
+            ### Inputs
+            
+            {convert_to_table(action_yaml['inputs'], is_inputs=True)}
+            
+            ### Outputs
+            
+            {convert_to_table(action_yaml['outputs'], is_inputs=False)}
+            """
+        )
+
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
