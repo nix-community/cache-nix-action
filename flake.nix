7c2a0cf6e6a49414ce07fd771237efc3b479458a
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-12-28 21:14:12 +0300

    add: nixConfig

--- flake.nix
+++ flake.nix
@@ -53,4 +53,16 @@
           inherit packages devShells;
         };
     };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+    keep-outputs = true;
+  };
 }
