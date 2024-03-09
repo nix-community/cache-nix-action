a9e25af7c3c93dfe892f8ae4c5e8ba50ec3bb123
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-03-09 22:23:19 +0300

    fix: remove actions-toolkit

--- actions-toolkit/packages/artifact/src/internal/shared/user-agent.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
-const packageJson = require('../../../package.json')
-
-/**
- * Ensure that this User Agent String is used in all HTTP calls so that we can monitor telemetry between different versions of this package
- */
-export function getUserAgentString(): string {
-  return `@actions/artifact-${packageJson.version}`
-}
