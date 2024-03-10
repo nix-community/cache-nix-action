8e41dc59fa6c14d7dea097195b8a7707cdb503f9
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-03-10 23:07:20 +0300

    fix(restore): --exclude-from file patterns

--- src/utils/action.ts
+++ src/utils/action.ts
@@ -80,7 +80,9 @@ export async function restoreCache({
     let extraTarArgs: string[] = [];
 
     if (!lookupOnly) {
-        const nixPaths = readdirSync("/nix/store").map(x => `/nix/store/${x}`);
+        const nixPaths = readdirSync("/nix/store").map(
+            x => `../../../../../nix/store/${x}`
+        );
         const tmp = await cacheUtils.createTempDirectory();
         const excludeFromFile = `${tmp}/nix-store-paths`;
         writeFileSync(excludeFromFile, nixPaths.join("\n"));
