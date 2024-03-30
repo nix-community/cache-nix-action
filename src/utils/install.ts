15b8453d6769c38295d8222b63211139d58ff427
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-03-30 17:58:46 +0300

    fix(action): check whether need sqlite3

--- src/utils/install.ts
+++ src/utils/install.ts
@@ -1,12 +1,13 @@
-import { which } from "which";
 import * as inputs from "../inputs";
 import { exec } from "@actions/exec";
 import * as utils from "./action";
+import { which } from "@actions/io";
 
 export async function installSQLite3() {
-    if (inputs.nix) {
-        const existsSqlite3 = await which("sqlite3");
-        if (!existsSqlite3) {
+    if (inputs.nix && inputs.isMacos) {
+        try {
+            await which("sqlite3", true);
+        } catch (error: unknown) {
             utils.info("No SQLite 3 found. Installing it.");
             exec(`brew install sqlite3`);
         }
