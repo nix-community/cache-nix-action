16139ff84feac23a5238fe9eb8ea8ac5daf4d63c
Danila Danko <br4ch1st0chr0n3@gmail.com>
2025-03-08 22:32:45 +0300

    refactor(action): use constants for target variants

--- action.nix
+++ action.nix
@@ -1,8 +1,12 @@
 { target, lib }:
 let
+  cache = "cache";
+  save = "save";
+  restore = "restore";
+
   specific =
     {
-      cache = {
+      "${cache}" = {
         name = "Restore and save";
         description = "Restore and save";
         actions = "restoring and saving";
@@ -11,14 +15,14 @@ let
           post: "dist/save/index.js"
             post-if: "success()"'';
       };
-      save = {
+      "${save}" = {
         name = "Save";
         description = "Save";
         actions = "saving";
         main = "../dist/save-only/index.js";
         post = "";
       };
-      restore = {
+      "${restore}" = {
         name = "Restore";
         description = "Restore";
         actions = "restoring";
@@ -66,7 +70,7 @@ in
       required: true
     
     ${
-      if target == "cache" || target == "restore" then
+      if target == cache || target == restore then
         ''
           ${restore-prefixes-first-match}:
               description: |
@@ -148,7 +152,7 @@ in
       required: false
 
     ${
-      if target == "cache" || target == "save" then
+      if target == cache || target == save then
         ''
           ${gc-max-store-size}:
               description: |
@@ -195,13 +199,17 @@ in
             purge-last-accessed:
               description: |
                 - ${effectOnlyWhen [ "purge: true" ]}
-                - When a non-negative number, the action purges selected caches that were last accessed more than this number of seconds ago relative to the start of the ${if target == "cache" then "`Post Restore` phase" else "Save step"}.
+                - When a non-negative number, the action purges selected caches that were last accessed more than this number of seconds ago relative to the start of the ${
+                  if target == cache then "`Post Restore` phase" else "Save step"
+                }.
                 - ${noEffectOtherwise}
               default: ""
             purge-created:
               description: |
                 - ${effectOnlyWhen [ "purge: true" ]}
-                - When a non-negative number, the action purges selected caches that were created more than this number of seconds ago relative to the start of the ${if target == "cache" then "`Post Restore` phase" else "Save step"}.
+                - When a non-negative number, the action purges selected caches that were created more than this number of seconds ago relative to the start of the ${
+                  if target == cache then "`Post Restore` phase" else "Save step"
+                }.
                 - ${noEffectOtherwise}
               default: ""
 
@@ -223,7 +231,7 @@ in
         - If you use a personal access token, it must have the `repo` scope ([link](https://docs.github.com/en/rest/actions/cache?apiVersion=2022-11-28#delete-github-actions-caches-for-a-repository-using-a-cache-key)).
       default: ''${{ github.token }}
   ${
-    if target == "cache" || target == "restore" then
+    if target == cache || target == restore then
       ''
         outputs:
           ${primary-key}:
