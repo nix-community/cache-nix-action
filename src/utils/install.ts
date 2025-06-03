f44ce8a9ca9172b4ab1fb03b9e687d35785babe6
Danila Danko <br4ch1st0chr0n3@gmail.com>
2025-06-03 20:08:34 +0300

    fix(action): install sqlite3 on all platforms if nix is enabled

--- src/utils/install.ts
+++ src/utils/install.ts
@@ -4,12 +4,43 @@ import * as inputs from "../inputs";
 import * as utils from "./action";
 
 export async function installSQLite3() {
-    if (inputs.nix && inputs.isMacos) {
+    if (inputs.nix) {
         try {
             await which("sqlite3", true);
         } catch (error: unknown) {
-            utils.info("No SQLite 3 found. Installing it.");
-            await utils.run(`brew install sqlite3`);
+            utils.info(
+                `
+                No SQLite 3 found.
+                
+                Trying to install it.
+                `
+            );
+
+            try {
+                utils.info(
+                    "Trying to install it using the experimental `nix` command."
+                );
+
+                await utils.run(`nix profile install nixpkgs#sqlite`);
+            } catch (error: unknown) {
+                try {
+                    utils.info(
+                        "Failed to install it using the experimental `nix` command."
+                    );
+
+                    utils.info(
+                        "Trying to install it using the `nix-env` command."
+                    );
+
+                    await utils.run(`nix-env --install sqlite`);
+                } catch (error: unknown) {
+                    throw new Error("Could not install SQLite 3.", {
+                        cause: error
+                    });
+                }
+            }
+
+            utils.info("Successfully installed SQLite 3!");
         }
     }
 }
