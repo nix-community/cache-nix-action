ac71065d3424befaaee61b18db6e01e3164ef3b0
Danila Danko <br4ch1st0chr0n3@gmail.com>
2025-12-25 12:02:23 +0300

    fix(ci): use newer runners

--- nix/ci.nix
+++ nix/ci.nix
@@ -44,6 +44,12 @@ let
               }}'';
 
   install-nix-action = ''- uses: cachix/install-nix-action@v31.9.0'';
+  os = {
+    ubuntu-24 = "ubuntu-24.04";
+    ubuntu-24-arm = "ubuntu-24.04-arm";
+    macos-14 = "macos-14";
+    macos-15 = "macos-15";
+  };
 in
 ''
   name: Nix CI ${name}
@@ -71,7 +77,7 @@ in
     # Commit and push the built code
     build:
       name: Build the action
-      runs-on: ubuntu-24.04
+      runs-on: ${os.ubuntu-24-arm}
       permissions:
         contents: write
         actions: write
@@ -144,10 +150,10 @@ in
       strategy:
         matrix:
           os:
-            - macos-14
-            - macos-15
-            - ubuntu-22.04
-            - ubuntu-24.04
+            - ${os.macos-14}
+            - ${os.macos-15}
+            - ${os.ubuntu-24}
+            - ${os.ubuntu-24-arm}
           id:
             - 1
             - 2
@@ -169,8 +175,8 @@ in
             extra_nix_config: |
               ''${{ env.extra_nix_config }}
               ''${{ (
-                    (matrix.id == 1 && matrix.os == 'ubuntu-22.04') || 
-                    (matrix.id == 2 && matrix.os == 'ubuntu-24.04')
+                    (matrix.id == 1 && matrix.os == '${os.macos-15}') || 
+                    (matrix.id == 2 && matrix.os == '${os.ubuntu-24-arm}')
                   ) && env.nix_config_ca_derivations || '''
               }}
         - name: Restore Nix store - ''${{ matrix.id }}
@@ -232,10 +238,10 @@ in
           strategy:
             matrix:
               os:
-                - macos-14
-                - macos-15
-                - ubuntu-22.04
-                - ubuntu-24.04
+                - ${os.macos-14}
+                - ${os.macos-15}
+                - ${os.ubuntu-24}
+                - ${os.ubuntu-24-arm}
           runs-on: ''${{ matrix.os }}
           steps:
             - name: Checkout this repo
@@ -324,10 +330,10 @@ in
             - true
             - false
           os:
-            - macos-14
-            - macos-15
-            - ubuntu-22.04
-            - ubuntu-24.04
+            - ${os.macos-14}
+            - ${os.macos-15}
+            - ${os.ubuntu-24}
+            - ${os.ubuntu-24-arm}
       runs-on: ''${{ matrix.os }}
       steps:
         - name: Checkout this repo
