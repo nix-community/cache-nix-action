dfde4d35b86aa2875e5829cfc8b6c2d4c203ab9b
Danila Danko <br4ch1st0chr0n3@gmail.com>
2026-01-30 21:33:34 +0300

    fix(action): choose command based on the Nix version

--- src/utils/collectGarbage.ts
+++ src/utils/collectGarbage.ts
@@ -2,6 +2,26 @@ import { Inputs } from "../constants";
 import * as inputs from "../inputs";
 import * as utils from "./action";
 
+interface NixVersion {
+    major: number;
+    minor: number;
+}
+
+function parseNixVersion(nixVersionStdout: string): NixVersion | undefined {
+    const regexMajorMinor = /(\d+)\.(\d+)/;
+
+    const match = nixVersionStdout.match(regexMajorMinor);
+
+    if (match) {
+        return {
+            major: parseInt(match[1], 10),
+            minor: parseInt(match[2], 10)
+        };
+    }
+
+    return undefined;
+}
+
 export async function collectGarbage() {
     utils.info("Removing useless files.");
 
@@ -10,9 +30,28 @@ export async function collectGarbage() {
     utils.info("Calculating store size.");
 
     async function getStoreSize() {
-        const { stdout } = await utils.run(
-            `nix --experimental-features nix-command path-info --json --json-format 2 --all | jq '.info | map(.narSize) | add'`
-        );
+        const nixVersionOutput = await utils.run(`nix --version`);
+
+        let nixVersion = parseNixVersion(nixVersionOutput.stdout);
+
+        if (nixVersion === undefined) {
+            utils.warning(
+                `Could not get Nix version ('nix --version'). Got: ${nixVersionOutput}.`
+            );
+            // We select commands based on the Nix version.
+            // Currently, we need to differentiate between
+            // >=2.33 versions and <2.33 versions.
+            // Versions after 2.33 definitely support `nix --version`.
+            // Therefore, let's assume the version is 2.0.
+            nixVersion = { major: 2, minor: 0 };
+        }
+
+        const nixCommand =
+            nixVersion.minor >= 33
+                ? `nix --experimental-features nix-command path-info --json --json-format 2 --all | jq '.info | map(.narSize) | add'`
+                : `nix --experimental-features nix-command path-info --json --all | jq 'map(.narSize) | add'`;
+
+        const { stdout } = await utils.run(nixCommand);
 
         const storeSize = (() => {
             try {
